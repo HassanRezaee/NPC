3f4df421d43dd9966dafac48c949bb9b0c7ea3b3
Hassan <h.rezaee@ut.ac.ir>
2021-08-31 12:41:49 -0400

    Add files via upload

---
+++ make_graph.R
@@ -0,0 +1,31 @@
+
+
+# Program to create an igraph graph object from the discretized road shape files
+
+# Inputs:
+ # roads_shp: road network shapefiles
+
+# Outputs: 
+ # igr: igraph graph object
+ # nodes_coords_fine: coordinates of all the nodes in igr
+
+
+
+make_graph = function(roads_shp){
+  
+  roads = roads_shp
+  roads = lines_midpoints(roads, return.dataframe = F, plot.results = F)
+  Sl_IDs <- sapply(slot(roads, "lines"), function(x) slot(x, "ID"))
+  data = data.frame(var=seq(from=1,to=length(roads),by=1))
+  rownames(data) = Sl_IDs
+  roads <- sp::SpatialLinesDataFrame(sl = roads, data = data, match.ID = T)
+  ea.prop = rep(1,length(roads)-1)
+  
+  network = shp2graph::readshpnw(roads, ELComputed=T, longlat=T, Detailed=F, ea.prop=ea.prop)
+  igr <- shp2graph::nel2igraph(network[[2]], network[[3]], weight=network[[4]],Directed = TRUE)
+  
+  nodelist = network[[2]]; edgelist = network[[3]]
+  nodes_coords_fine = shp2graph::Nodes.coordinates(nodelist)
+
+  return(list(igr,nodes_coords_fine))
+}
